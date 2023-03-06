4d148a1943a4300c55794cd52f0bf75ecb311b3c
fmiah10 <[fmiah10@stuy.edu]>
2023-03-06 16:00:33 -0500

    Updated cube.py to have each face a matrix

--- cube.py
+++ cube.py
@@ -1,1 +1,32 @@
 # This program positions the cube and helps it do certain movements
+
+upFace = [
+    ['W', 'W', 'W'],
+    ['W', 'W', 'W'],
+    ['W', 'W', 'W']
+]
+downFace = [
+    ['Y', 'Y', 'Y'],
+    ['Y', 'Y', 'Y'],
+    ['Y', 'Y', 'Y']
+]
+rightFace = [
+    ['G', 'G', 'G'],
+    ['G', 'G', 'G'],
+    ['G', 'G', 'G']
+]
+leftFace = [
+    ['B', 'B', 'B'],
+    ['B', 'B', 'B'],
+    ['B', 'B', 'B']
+]
+frontFace = [
+    ['O', 'O', 'O'],
+    ['O', 'O', 'O'],
+    ['O', 'O', 'O']
+]
+backFace = [
+    ['R', 'R', 'R'],
+    ['R', 'R', 'R'],
+    ['R', 'R', 'R']
+]
